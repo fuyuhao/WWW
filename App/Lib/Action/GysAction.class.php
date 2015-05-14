979e525661dc0f4e9f822d4b214dc60a2b2ddce5
anticoder <309051607@qq.com>
2015-05-14 10:48:54 +0800

    完成用户审核

--- App/Lib/Action/GysAction.class.php
+++ App/Lib/Action/GysAction.class.php
@@ -16,7 +16,23 @@ class GysAction extends BaseAction {
     public function index() {
         $this->display();
     }
+	
+	public function getData() {
+        $TypeModel = D('UserView');
+        $dataList = $TypeModel->order('uid asc,ustatus asc')->select();
+        $this->returnGridData($dataList, $TypeModel->count());
+    }
+	
+	public function doupdate() {
+		$did = $_GET['uid'];
+		if ($did) {
+            $Model = D("User");
+			$Model->where('uid = %d', $did)->save(array('ustatus' => 1));
 
+        }
+        $this->returnStatus();
+		
+	}
 
 
 }
---
+++ App/Lib/Model/UserViewModel.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * 作者：anticode
+ * 
+ * 
+ */
+
+/**
+ * UserViewModel
+ *
+ * @author anticode
+ */
+class UserViewModel extends ViewModel {
+
+    protected $viewFields = array(
+        'User' => array('uid','uname', 'account', 'mail','ustatus')
+    );
+
+}
+
+?>
